b8a53a6d55b0eb05cc41988fe371690fc18ecfe1
Caroline Kok <ckok@thoughtworks.com>
2015-10-03 13:01:28 +0100

    Caroline: added grid system.

---
+++ model/grid.js
@@ -0,0 +1,27 @@
+var _ = require('lodash');
+
+module.exports = function(gridSize) {
+
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U','V', 'W', 'X', 'Y', 'Z'];
+
+    var columnSize = alphabet.indexOf(gridSize.slice(0,1)) + 1;
+    var rowSize = parseInt(gridSize.slice(1, gridSize.length));
+
+    var grid = {};
+
+    _.each(alphabet.slice(0, columnSize), function(column) {
+        for (i = 1; i < rowSize + 1; i++) {
+            var position = column.toString() + i;
+            grid[position] = "not discovered";
+        }
+    });
+
+    self.getPosition = function(position) {
+        return grid[position];
+    };
+
+    self.setPosition = function(position, information) {
+        grid[position] = information;
+    }
+};
